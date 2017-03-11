951f4f6ace119cc546872e350c1ccd25c5e4c252
ttonnellier <nastyk.fr@gmail.com>
2017-03-11 21:49:07 +0100

    Add the two isr.

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 ### Download required libraries
 [See here](http://playground.arduino.cc/Code/Timer1 "Arduino Playground") 
 to download and install TimerOne and TimerThree libraries
+
+Or (simplier) use Sketch > Import Library > Manage Library 
+and enter timerone in the search field.
--- eole.ino
+++ eole.ino
@@ -1,3 +1,5 @@
+#include <TimerOne.h>
+
 #define DEBUG
 
 #define PWM_MOTOR OCR2A 
@@ -6,7 +8,8 @@
 #define PIN_P_SER 3     // digital pin (out)
 #define DIR_MOTOR 12    // digital pin (out) : polulu dir
 
-#define PIN_SWITCH 1   //digital pin (in) switch
+#define PIN_SWITCH 1    // digital pin (in) switch
+#define PIN_ANEMO  2    // digital pin (in) anemometer
 
 #define POLULU_HIGH 13
 #define POLULU_LOW  10
@@ -26,11 +29,40 @@
 #define SPEED_STEP  2
 #define OFFSET_MOTOR 0
 #define OFFSET_GIROU 0
+#define ROT_MAX 50
+#define DEBOUNCE_MS 10 
 
 char sens;
 short dist;
 unsigned short dist_abs, pos_mot, pos_gir;
-bool flag_servo;
+
+volatile bool flag_servo;
+volatile unsigned int rotation;
+volatile unsigned int timer_count; 
+volatile unsigned long debounce;
+
+// interruption Timer1
+void isr_timer() {
+    timer_count++; //toutes les 0.5s
+    if(timer_count == 10) // Toutes les 5s
+    {
+#ifdef DEBUG    
+        Serial.print ("rotation value: "); Serial.println(rotation);
+#endif
+        if (rotation > ROT_MAX)
+            flag_servo = true;
+        rotation    = 0;
+        timer_count = 0;
+    }
+}
+
+// interruption rotation anemo
+void isr_rotation() {
+    if((millis() - debounce) > DEBOUNCE_MS ) { // debounce the switch contact.
+        rotation++;
+        debounce = millis();
+    }
+}
 
 
 inline void calc_sens() {
@@ -125,11 +157,8 @@ void control_motor_reverse() {
     PWM_MOTOR = SPEED_MIN_R;
 
 #ifdef DEBUG
-    pos_mot = (analogRead(POT_MOTOR)+OFFSET_MOTOR)&1023;
-    Serial.print("**Fin** pos_gir: "); 
-    Serial.print(pos_gir);
-    Serial.print("pos_mot: "); 
-    Serial.println(pos_mot);
+    pos_mot = (analogRead(POT_MOTOR)+OFFSET_MOTOR)&1023; 
+    Serial.print("**Fin** pos_gir: "); Serial.print(pos_gir); Serial.print("pos_mot: "); Serial.println(pos_mot);
 #endif
 }
 
@@ -142,22 +171,32 @@ void setup() {
     pinMode(POLULU_HIGH, OUTPUT);
     pinMode(PIN_P_SER  , OUTPUT);
 
+    // Switch on bloquant
     pinMode(PIN_SWITCH , INPUT );
-    // pas de config pour in car analog 
+    // pas de config pour potard car analog 
 
     // init Polulu
     digitalWrite(DIR_MOTOR  ,LOW );
     digitalWrite(POLULU_LOW ,LOW );
     digitalWrite(POLULU_HIGH,HIGH);
     PWM_MOTOR = SPEED_MIN_N;
-    TCCR2A = 0b10100011; //f pwm= 7kHz
-    TCCR2B = 0b00000010;
+    TCCR2A    = 0b10100011; //f pwm= 7kHz
+    TCCR2B    = 0b00000010;
     PWM_MOTOR = SPEED_MIN_N;
 
     // init servo
     PWM_SERVO  = SERVO_ENCLANCHE;
     flag_servo = false;
 
+    //init anemo
+    rotation    = 0;
+    debounce    = 0;
+    timer_count = 0;
+    pinMode(PIN_ANEMO, INPUT);
+    attachInterrupt(digitalPinToInterrupt(PIN_ANEMO), isr_rotation, FALLING);
+
+    Timer1.initialize(500000); //0.5s
+    Timer1.attachInterrupt(isr_timer); 
 
 #ifdef DEBUG    
     Serial.begin(9600);
